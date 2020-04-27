cec591fdfa1791e3a2a05f87349150f84b0bd6a8
93geraldtee <93geraldtee@gmail.com>
2020-04-27 16:12:39 -0400

    add seeds

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Roaster.create([
+    {
+        name: "Roast'd",
+        location: "Fort Lee & Hasbrouck Heights, NJ"
+    }
+])
+Roaster.create([
+    {
+        name: "Stray Cat Brew",
+        location: "Maywood, NJ"
+    }
+])
+Roaster.create([
+    {
+        name: "American Bulldog Coffee Roasters",
+        location: "Ridgewood & Ramsay, NJ & Chestnut Ridge, NY"
+    }
+])
+
+Coffee.create([{
+    name: "Kivu - Rwanda",
+    notes: "Rhubarb, honey, smooth. Certified Fair Trade",
+    roast: "Dark",
+    roaster_id: 1
+}])
+Coffee.create([{
+    name: "Peruvian HB Cuzco Region",
+    notes: "Full-bodied, citrus notes. Delicate, with floral and nutty overtones and a sweet finish. Fruity acidity.",
+    roast: "Medium",
+    roaster_id: 2
+}])
+Coffee.create([{
+    name: "Bali Blue Moon",
+    notes: "Bakers Chocolate, Licorice and Red Wine",
+    roast: "medium/dark",
+    roaster_id: 3
+}])
+
+p "Created #{Roaster.count} roasters"
+p "Created #{Coffee.count} coffees"
